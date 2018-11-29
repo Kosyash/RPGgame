2c3f930ce6523e39d6f1915ee7f914a2597a0a7b
Kosyash <45121900+Kosyash@users.noreply.github.com>
2018-11-29 14:16:02 +0300

    Update monster.py
    Комментирование "Монстра"

--- monster.py
+++ monster.py
@@ -1,17 +1,15 @@
-class Monster:
-  def __init__(self,name,mtype,straight):
+class Monster: #Объявлен класс монстер
+  def __init__(self,name,mtype,straight): #Конструктор класса - имя, тип, сила
     self.name=name
     self.mtype=mtype
     self.x=0
-    
     self.y=0
-    
     self.hp=100
     self.straight=straight
-  def say(self,text):
+  def say(self,text): #Метод речи
     self.text=text
-  def get_damage(self):
+  def get_damage(self): #Метод вычисления наносимого урона
     self.damage=((self.straight**2)/2)*1.5
     return self.damage
-  def attack(self):
+  def attack(self): #Метод атаки
     return self.get_damage()/2
